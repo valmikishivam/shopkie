41c9270b32d32ad745c730e33ce1e5e8d3e63f50
shivam valmiki <shivamvalmiki976@gmail.com>
2026-06-02 14:17:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,7 @@ const app= express();
 const port = process.env.PORT||3030;
 app.use(express.json());
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
+    origin:process.env.FRONTEND_URL
 }));
 
 connectDb()
