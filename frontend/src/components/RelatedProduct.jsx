392e4e51391b059ff305b77b066b912dbebb5d84
shivam valmiki <shivamvalmiki976@gmail.com>
2026-06-02 13:22:03 +0530

    Update RelatedProduct.jsx
    add loader

--- frontend/src/components/RelatedProduct.jsx
+++ frontend/src/components/RelatedProduct.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Loader } from 'lucide-react'
 import ProductCard from './ProductCard.jsx'
 
 const RelatedProduct = ({ category }) => {
   const [data, setData] = useState([])
-
+  const [loading,setLoading] = useState(false)
   useEffect(() => {
     const reletedData = async () => {
 
       try {
+        setLoading(true)
         const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/shopkie/related?category=${category}`);
         setData(res.data.products);
-
       } catch (error) {
         console.log(error);
+      }finally{
+        setLoading(false);
       }
     }
     reletedData()
@@ -21,9 +24,13 @@ const RelatedProduct = ({ category }) => {
   return (
     <div className='w-full grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
       {
-        data && data.map(pro => (
+        data ?data.map(pro => (
           <ProductCard data={pro} key={pro.uid} />
-        ))
+        )):loading?(
+          <div className='col-span-3 md:col-span-4 lg:col-span-6 place-items-center'>
+         <Loader size={30} className='animate-spin' />
+          </div>
+        ):(<h1>no products</h1>)
       }
     </div>
   )
